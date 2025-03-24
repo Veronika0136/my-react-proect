5a8686c37960a8baa57df0bf0c123c782310e2f4
veronika <artemova331@gmail.com>
2025-03-24 21:29:09 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import "modern-normalize";
-import App from './components/App'
+// import App from './components/App'
 import './components/App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
   </StrictMode>,
 )
