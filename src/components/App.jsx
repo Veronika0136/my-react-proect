f3903409595f077d12393e0106655a5f32b73c67
veronika <artemova331@gmail.com>
2025-05-19 19:57:02 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import css from './App.module.css';
 
+const App = () => {
+  return <div className={css}>App </div>;
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "modern-normalize";
-// import App from './components/App'
-import './components/App.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import 'modern-normalize';
+import App from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
